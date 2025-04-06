b71da3bf29c69e4fa0b07c0da3f1c76f73931561
Miho Asai <miffish18@gmail.com>
2025-04-07 01:01:47 +0900

    fix: volumes

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import { cn } from "@/lib/admin/utils";
 
 const notoSansJP = Noto_Sans_JP({
--- frontend/src/app/admin/login/page.tsx
+++ frontend/src/app/admin/login/page.tsx
@@ -2,23 +2,23 @@
 import React from 'react'
 import Link from "next/link"
 import { Button } from "../../../components/shadcn/ui/button";
-import { Calendar } from "../../../components/shadcn/ui/calendar";
+// import { Calendar } from "../../../components/shadcn/ui/calendar";
 import { cn } from "../../../lib/admin/utils";
 
 
 export default function Home() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date())
+  // const [date, setDate] = React.useState<Date | undefined>(new Date())
   return (
     <><header className="w-full p-4 flex justify-end">
       <Button asChild>
         <Link href="/auth/login">管理者ログイン</Link>
       </Button>
     </header><main className={cn()}>
-        <Calendar
+        {/* <Calendar
           mode="single"
           selected={date}
           onSelect={setDate}
-          className="rounded-md border bg-neutral-100" />
+          className="rounded-md border bg-neutral-100" /> */}
       </main></>
   );
 }
