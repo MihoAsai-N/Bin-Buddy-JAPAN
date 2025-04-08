72626aadedc095111eb5cb5c92f979c9498b514d
Miho Asai <miffish18@gmail.com>
2025-04-08 16:19:31 +0900

    update: invoice_link

--- frontend/src/app/admin/(authenticated)/dashboard/page.tsx
+++ frontend/src/app/admin/(authenticated)/dashboard/page.tsx
@@ -1,5 +1,21 @@
 // frontend/src/app/admin/dashboard/page.tsx
 
+"use client"
+
+import { Button } from "@/app/admin/components/shadcn/ui/button"
+
 export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold mb-4">Dashboard Page</h1>
+
+      <a
+        href="https://invoice.stripe.com/i/acct_1RBRhUDtvvwujPXx/test_YWNjdF8xUkJSaFVEdHZ2d3VqUFh4LF9TNWRKZ3RhbVBFcFo3eUsyVTk2aFNMYWRZT0dhNFJ3LDEzNDYyMDc5NQ020058NDrwjY?s=db"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button variant="default">請求書を表示</Button>
+      </a>
+    </div>
+  )
 }
