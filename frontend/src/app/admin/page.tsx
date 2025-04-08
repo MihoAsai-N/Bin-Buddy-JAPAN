312a4c7b96b245999978ef779f9cbdafe205fca6
Miho Asai <miffish18@gmail.com>
2025-04-08 10:28:18 +0900

    update: screen_transition

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,5 +1,18 @@
-// frontend/src/app/admin/page.tsx
+'use client'
 
-export default function AdminPage() {
-  return <div>Admin Page</div>;
+import React from 'react'
+import Link from 'next/link'
+import { Button } from './components/shadcn/ui/button'
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold mb-4">WEB版</h1>
+      <Link href="/admin/login">
+        <Button>管理者画面</Button>
+      </Link>
+    </div>
+  )
 }
+
+export default Page
