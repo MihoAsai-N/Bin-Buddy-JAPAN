8244da36ac3920740fb870473d0e8fc4c3b05e25
Miho Asai <miffish18@gmail.com>
2025-04-13 18:52:05 +0900

    fix: filename_checkout.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 
 # 既存ルーターの読み込み
-from routers import classify, stripe
+from routers import classify, checkout
 
 # .envファイルの読み込み
 load_dotenv()
@@ -24,7 +24,7 @@ app.add_middleware(
 
 # ルーターをアプリケーションに含める
 app.include_router(classify.router)
-app.include_router(stripe.router)
+app.include_router(checkout.router)
 
 
 if __name__ == "__main__":
