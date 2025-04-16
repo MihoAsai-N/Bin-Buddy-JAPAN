e4ffd9381c1b54703d6c558c19e49c149f8e80c7
MadokaSasaki <msasaki11142@gmail.com>
2025-04-17 03:38:27 +0900

    カレンダー情報無限ロープ解除

--- frontend/src/app/calendar/page.tsx
+++ frontend/src/app/calendar/page.tsx
@@ -4,21 +4,12 @@ import React, { useState, useEffect } from "react";
 import { Navigation } from "../components/navigation";
 import { Button } from "../components/ui/button";
 import { useLanguage } from "../contexts/language-context";
-import { useRouter, useSearchParams } from "next/navigation";
+import {useSearchParams } from "next/navigation";
 import {
-  useTrash,
-  type TrashType,
-  type WeekDay,
-} from "../contexts/trash-context";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../components/ui/tooltip";
+  useTrash, WeekDay } from "../contexts/trash-context";
+import { TooltipProvider} from "../components/ui/tooltip";
 import { NavLinks } from "../components/nav-links";
 import { VisionResult } from "../components/VisionResult";
-import { getTrashIcon as getExternalTrashIcon } from "../lib/icons";
 import Legend from "../components/Legend";
 import {
   DropdownMenu,
@@ -26,7 +17,6 @@ import {
   DropdownMenuTrigger,
 } from "../components/ui/dropdown-menu";
 import { FaTrashAlt } from "react-icons/fa";
-import axios from "axios";
 import IrregularComment from "../components/IrregularComment";
 import { getGarbageDay } from "../lib/utils";
 import { getTrashIcon } from "./AddIcon";
@@ -48,27 +38,31 @@ const getWeekdayFromNumber = (num: number): WeekDay => {
 export default function CalendarPage() {
   const [selectedDate] = useState(new Date());
   const { t, language } = useLanguage();
-  const { region, getTrashTypesForWeekday } = useTrash();
-  const [isLegendOpen] = useState(false);
+  const { getTrashTypesForWeekday } = useTrash();
+  // const [isLegendOpen] = useState(false);
   const searchParams = useSearchParams();
   const area = searchParams.get("area");
   const [calendarData, setCalendarData] = useState<
     { [key: string]: string | null }[] | null
   >(null);
 
-  console.log("areaの値：", area);
-
+  console.log("areaの値：", area);//TODO:確認後削除
   useEffect(() => {
     console.log("areaの値：", area);
     fetch(`/api?area=${encodeURIComponent(area || "")}`, { cache: "no-store" })
       .then((response) => response.json())
       .then((data) => {
-        // 変数名を 'result' から 'data' に変更
         console.log("API レスポンス:", data);
         if (data && data.success && data.data) {
-          const records = data.data;
-          console.log("取得したデータレコード:", records);
-          setCalendarData(records);
+          const newRecords = data.data;
+          console.log("取得したデータレコード:", newRecords);
+  
+          // 現在の calendarData と新しい records を比較
+          if (JSON.stringify(calendarData) !== JSON.stringify(newRecords)) {
+            setCalendarData(newRecords);
+          } else {
+            console.log("データは変更されていません。");
+          }
         } else {
           console.error("データの取得に失敗しました:", data);
         }
@@ -77,6 +71,7 @@ export default function CalendarPage() {
         console.error("Fetch エラー:", error);
       });
   });
+
 
   // カレンダーの日付を生成
   const generateCalendarDays = () => {
@@ -142,19 +137,12 @@ export default function CalendarPage() {
         garbageDayValue: null,
       });
     }
-
     return days;
   };
 
   const days = generateCalendarDays();
   const weekdays = ["S", "M", "T", "W", "T", "F", "S"];
 
-  const getTrashName = (trashType: TrashType) => {
-    return t(`result.${trashType}`);
-  };
-  const toggleLegend = () => {
-    setIsLegendOpen(!isLegendOpen);
-  };
   return (
     <div className="flex flex-col min-h-screen">
       <Navigation />
@@ -190,41 +178,6 @@ export default function CalendarPage() {
                     {day}
                   </div>
                 ))}
-
-                {/* {days.map((day, i) => (
-                  <div
-                    key={i}
-                    className={`relative px-0 mb-1 py-0 pb-2 mt-2 h-14 flex flex-col items-center text-lg font-semibold justify-start ${
-                      day.selected ? "bg-[#cbe8ed] rounded-full" : ""
-                    } ${!day.current ? "text-gray-300" : ""}`}
-                  >
-                    <span className="mb-1">{day.day}</span>
-
-                    {day.current && (
-                      <div className="flex flex-col items-center mt-auto">
-                        {day.trashTypes && day.trashTypes.length > 0 && (
-                          <div className="flex justify-center space-x-0.5">
-                            {day.trashTypes.map((trashType, index) => (
-                              <Tooltip key={index}>
-                                <TooltipTrigger asChild>
-                                  <div>{getExternalTrashIcon(trashType)}</div>
-                                </TooltipTrigger>
-                                <TooltipContent>
-                                  <p>{getTrashName(trashType)}</p>
-                                </TooltipContent>
-                              </Tooltip>
-                            ))}
-                          </div>
-                        )}
-                        {day.garbageDayValue !== null && (
-                          <div className="mt-1">
-                            {getTrashIcon(day.garbageDayValue)}
-                          </div>
-                        )}
-                      </div>
-                    )}
-                  </div>
-                ))} */}
                 {days.map((day, i) => (
                   <div
                     key={i}
