ae2fb003636f114854587001499e6aedc77d322b
MariMONO <mariccori@gmail.com>
2025-04-24 14:34:54 +0900

    [fix]DB接続を.envを使用する形で変更

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,9 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-# DB URL（例: PostgreSQL）
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/mydb"
+# .envを読み込む（忘れがちなので上の方で）
+load_dotenv()
+
+# .env からDB接続情報を取得
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+# SQLAlchemyの接続URLを組み立てる
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # 接続設定
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
