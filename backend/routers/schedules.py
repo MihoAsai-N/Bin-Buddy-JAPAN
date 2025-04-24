29972c9c3bcc0cce0ddc68fd4664585d20bcf3b9
Miho Asai <miffish18@gmail.com>
2025-04-24 16:14:53 +0900

    fix: mockdata

--- backend/routers/schedules.py
+++ backend/routers/schedules.py
@@ -15,9 +15,11 @@ mock_schedules = [
     {"id": "3", "districtId": "1", "areaId": "1", "day": "水曜日", "garbageTypeId": "3"},
     {"id": "4", "districtId": "1", "areaId": "1", "day": "木曜日", "garbageTypeId": "4"},
     {"id": "5", "districtId": "1", "areaId": "1", "day": "金曜日", "garbageTypeId": "5"},
-    {"id": "6", "districtId": "2", "areaId": "4", "day": "月曜日", "garbageTypeId": "1"},
-    {"id": "7", "districtId": "2", "areaId": "4", "day": "火曜日", "garbageTypeId": "2"},
-    {"id": "8", "districtId": "2", "areaId": "4", "day": "水曜日", "garbageTypeId": "3"},
+    {"id": "6", "districtId": "1", "areaId": "2", "day": "月曜日", "garbageTypeId": "1"},
+    {"id": "7", "districtId": "1", "areaId": "2", "day": "水曜日", "garbageTypeId": "2"},
+    {"id": "8", "districtId": "2", "areaId": "4", "day": "月曜日", "garbageTypeId": "1"},
+    {"id": "9", "districtId": "2", "areaId": "4", "day": "火曜日", "garbageTypeId": "2"},
+    {"id": "10", "districtId": "2", "areaId": "4", "day": "水曜日", "garbageTypeId": "3"},
 
 
 ]
